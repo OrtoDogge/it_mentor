48c4d3d1711f1d7c09414f71f50e739f18f226ce
Nikita Sukhorutchenko <134293371+OrtoDogge@users.noreply.github.com>
2024-11-13 17:16:16 +0400

    Update Project #92 - Calculator.py
    Правки с выводом сообщений об ошибке.

--- Project #92 - Calculator.py
+++ Project #92 - Calculator.py
@@ -1,7 +1,14 @@
 def main(input: str):
     l = input.split()
-    if len(l) != 3 or False in (l[0].isdigit(), l[2].isdigit()) or int(l[0]) > 10 or int(l[0]) < 1 or int(l[2]) < 1 or int(l[2]) > 10 or l[1] not in '-+/*':
-            raise Exception
+    if len(l) != 3:
+        print('Неверный формат ввода')
+        raise Exception
+    if False in (l[0].isdigit(), l[2].isdigit()) or int(l[0]) > 10 or int(l[0]) < 1 or int(l[2]) < 1 or int(l[2]) > 10:
+        print('Используйте только числа от 1 до 10 включительно')
+        raise Exception
+    if l[1] not in '-+/*':
+        print('Разрешены только операции сложения, вычитания, умножения и деления')
+        raise Exception
     return int(eval(input))
 
 while True:
